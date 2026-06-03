b6d1ea57749f7c3fd53a63df1aafdee93c2892b8
appoTech <tech@appopener.com>
2026-06-03 20:00:38 +0530

    fix: resolve .env 404 error on Vercel by migrating configuration to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyCg9ywrakadw11ql2Gl1-Ywc6r6_KDQZAY",
+  authDomain: "madpm-6a036.firebaseapp.com",
+  projectId: "madpm-6a036",
+  storageBucket: "madpm-6a036.firebasestorage.app",
+  messagingSenderId: "40453504945",
+  appId: "1:40453504945:web:52c0927e2f5f0bb5664790"
+};
